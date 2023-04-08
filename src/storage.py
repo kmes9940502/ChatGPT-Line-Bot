8ae95c400774b1d854abafeff73754848b835ac6
官澔 <kuan@guanhaodeMacBook-Air.local>
2023-04-08 21:43:16 +0800

    新增會員功能

---
+++ src/storage.py
@@ -0,0 +1,94 @@
+import json
+import datetime
+
+
+class FileStorage:
+    def __init__(self, file_name):
+        self.fine_name = file_name
+        self.history = {}
+
+    def save(self, data):
+        self.history.update(data)
+        with open(self.fine_name, 'w', newline='') as f:
+            json.dump(self.history, f)
+
+    def load(self):
+        with open(self.fine_name, newline='') as jsonfile:
+            data = json.load(jsonfile)
+        self.history = data
+        return self.history
+
+
+class MongoStorage:
+    def __init__(self, db):
+        self.db = db
+
+    def save(self, data):
+        user_id, api_key = list(data.items())[0]
+        self.db['api_key'].update_one({
+            'user_id': user_id
+        }, {
+            '$set': {
+                'user_id': user_id,
+                'api_key': api_key,
+                'created_at': datetime.datetime.utcnow()
+            }
+        }, upsert=True)
+
+    def GetUserAPIKey(self, id):
+        res = self.db['api_key'].find_one({'user_id':id})
+        if res:
+            return res['api_key']
+        else:
+            return "Error"
+        
+    def IsInDatabase(self, id):
+        res = self.db['api_key'].find_one({'user_id':id})
+        if res:
+            return True
+        else:
+            return False
+    def GetMember(self, data):
+        res = self.db['api_key'].find_one({'user_id':id})
+        if res:
+            return res['is_member']
+        else:
+            return False
+
+    def SetMember(self, data):
+        user_id = data
+        self.db['api_key'].update_one({
+            'user_id': user_id
+        }, {
+            '$set': {
+                'is_member': True,
+            }
+        }, upsert=True)       
+        
+    def DeleteMember(self, data):
+        user_id = data
+        self.db['api_key'].update_one({
+            'user_id': user_id
+        }, {
+            '$set': {
+                'is_member': False,
+            }
+        }, upsert=True)
+
+    def load(self):
+        data = list(self.db['api_key'].find())
+        res = {}
+        for i in range(len(data)):
+            res[data[i]['user_id']] = data[i]['api_key']
+        return res
+
+
+class Storage:
+    def __init__(self, storage):
+        self.storage = storage
+
+    def save(self, data):
+        self.storage.save(data)
+
+    def load(self):
+        return self.storage.load()
